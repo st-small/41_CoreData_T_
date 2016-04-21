ed18756ec624f3cc99a121ac5c660d72014a7ddf
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-04-21 10:41:37 +0300

    Initial Commit

---
+++ 41_CoreData_T_/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  41_CoreData_T_
+//
+//  Created by Stas on 21.04.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ 41_CoreData_T_/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  41_CoreData_T_
+//
+//  Created by Stas on 21.04.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
